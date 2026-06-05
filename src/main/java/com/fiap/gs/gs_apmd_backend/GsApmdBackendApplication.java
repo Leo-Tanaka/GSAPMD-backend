4121998554ebbd8cd2463db67c7e963bdc5523d8
TanakaLeo <leonardotanaka0513@gmail.com>
2026-06-04 21:27:58 -0300

    chore: estrutura inicial gerada pelo Spring Initializr

---
+++ src/main/java/com/fiap/gs/gs_apmd_backend/GsApmdBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.gs.gs_apmd_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsApmdBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsApmdBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/gs/gs_apmd_backend/GsApmdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.gs.gs_apmd_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsApmdBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
