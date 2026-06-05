7397dca202bb6484af4caa2103bd75d15c663187
TanakaLeo <leonardotanaka0513@gmail.com>
2026-06-05 18:09:14 -0300

    feat: implementa interface SensorDataRepository

---
+++ src/main/java/com/fiap/gs/gs_apmd_backend/repository/SensorDataRepository.java
@@ -0,0 +1,10 @@
+package com.fiap.gs.gs_apmd_backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fiap.gs.gs_apmd_backend.model.SensorData;
+
+@Repository
+public interface SensorDataRepository extends JpaRepository<SensorData, Long> {
+}
